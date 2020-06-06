31ba14301d885953010591163852b56d8c149b04
vinicius-16 <vinicius16@github.com>
2020-06-06 19:49:50 -0300

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData=[
 	{name:'Mark'},
@@ -13,7 +16,6 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
-
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -23,6 +25,26 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
-app.listen(8000,function(){
-    console.log('server is listening')
+app.post('/login',function(req,res){
+	const username=req.body.username;
+	const password=req.body.password;
+
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
 })
+
+app.listen(8000,function(){console.log('server is listening')})
